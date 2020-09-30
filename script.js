8e2f5b9ed2e485ddcd10e99e22e294ee8a35be36
Dana Kuglin <dakuglin@gmail.com>
2020-09-29 20:07:50 -0600

    updated files, added timer to quiz

--- script.js
+++ script.js
@@ -2,36 +2,38 @@
 //----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 var questionArray = [ // object were I am storing all of the questions, button informaiton, and answers
-    
+
     { 
     question: "What is the correct symbol for adding comments in JavaScript?",
-    btns: ["//", "<!--", "/*", "!!"],
+    buttons: ["//", "<!--", "/*", "!!"],
     answer: "correct Q1",
     },
         
     {
     question: "Question 2?",
-    btns: ["incorrect", "incorrect","correct Q2", "incorrect"],
+    buttons: ["incorrect", "incorrect","correct Q2", "incorrect"],
     answer: "correct Q2",
     },
             
     {
     question: "Question 3?",
-    btns: ["incorrect", "correct Q3", "incorrect", "incorrect"],
+    buttons: ["incorrect", "correct Q3", "incorrect", "incorrect"],
     answer: "correct Q3",
     },
             
     {
     question: "Question 4?",
-    btns: ["incorrect", "incorrect", "incorrect", "correct Q4"],
+    buttons: ["incorrect", "incorrect", "incorrect", "correct Q4"],
     answer: "correct Q4",
     },
         
 ]
 
 var currentQuestionIndex = 0;
+var secondsLeft = 30;
 
-
+//var currentQuizQuestion = questionArray[currentQuestionIndex];
+// console.log(currentQuizQuestion);
 
 // Functions 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -50,44 +52,129 @@ function startQuiz() {
     contentStartQuiz = "visible";
     
   
-    questionButtonGenerator() // call this function to start the quiz after the user presses the start button
-    buttons()
+    questions() // call this function to dispaly questions after the user presses the start button
+    buttons() // call this function to display buttons after the user presses the start button
 };
 
 startQuizBtn.addEventListener("click", startQuiz); // when the user clicks the start quiz button my quiz will start
 
-//startQuiz();
 
-function questionButtonGenerator() {
+function questions() {
 
-    var currentQuestionIndex = 0; //global variable above, starting at 0 index
+    //var currentQuestionIndex = 0; //global variable above, starting at 0 index
 
-    var currentQuizQuestion = questionArray[currentQuestionIndex];
-    //console.log(currentQuizQuestion);
+    var currentQuizQuestion = questionArray[currentQuestionIndex].question;
+    console.log(currentQuizQuestion);
 
     var questionElement = document.getElementById("questions"); // variable for where I will dynamically access the <h2> tag where the questions will go
     questionElement.textContent = currentQuizQuestion;
-    console.log( questionElement.textContent);
+
+};
+
+
+
+function buttons() {
+
+    var currentButtons = questionArray[currentQuestionIndex].buttons;
+    console.log(currentButtons);
+     
+    currentButtons.forEach(function(buttons) {  // specifically for iterating over an array (.forEach)
+
+        var dynamicButtons = document.createElement("BUTTON");
+        dynamicButtons.setAttribute("dynamic-buttons", buttons)
+        dynamicButtons.textContent = buttons;
+
+        dynamicButtons.onclick = handleDynamicBtns()
+    
+        document.body.appendChild(dynamicButtons);
+       
+        buttons.onclick = handleDynamicBtns();
+
+    })
+
+};
+
+
+function handleDynamicBtns() {
+
+    var answer = questionArray[currentQuestionIndex].answer;
+
+    console.log(answer);
+     if (this.value !== questionArray[currentQuestionIndex].answer) { //.this refers to my questionArray object
+       
+
+        var alert = document.getElementById("correct-incorrect");
+        console.log(alert);
+        alert.textContent = "Incorrect!"
+     } else {
+         alert.textContent = "Correct!"
+     }
+
+      currentQuestionIndex++;
+
+      //return currentQuestionIndex;
+       
+
+ };
+
+
+function quizEnd() {
+
+    if (quizTime ===0) {
+        
+    }
+
+};
+
+
+
+    
+    timeEl = document.getElementById("time");
+
+    var countdown = setInterval(function(){
+        secondsLeft--; //decrease time by 1 second
+        timeEl.textContent = secondsLeft
+
+        if(secondsLeft ===0) {
+            clearInterval(timeEl);
+        }
+    }, 1000);
+
+
+    
+    
+    
+//     document.log(countdown);
+
+//     quizTime--;
+//     countdown.textContent = time;
+  
+//     // check if user ran out of time
+//     if (time <= 0) {
+//       quizEnd();
+//     }
+//   }
 
 
 
 
 
-    var currentQuizQuestion = questionArray[currentQuestionIndex];
-    console.log(currentQuizQuestion);
+ // for (var i=0; i < questionArray.length; i++); {
+
+    //     var dynamicButtons = questionArraydocument.createElement("BUTTON");
+    //     console.log(dynamicButtons)
+    //     dynamicButtons.setAttribute("btnQuestions",buttons)
+    //     dynamicButtons.textContent = buttons;
+    //     dynamicButtons.innerHTML = currentButtons;
+    //     console.log(dynamicButtons.innerHTML)
+    //     document.body.appendChild(dynamicButtons);
+
+    // }
+
+
+
     
 
-    currentQuizQuestion.btns.forEach(function(btns) {  // specifically for iterating over an array (.forEach)
-
-    console.log(btns);
-   // var button = questionArray.btns
-    console.log(btns);
-    var btnTag = document.createElement("BUTTON");
-    btnTag.setAttribute("btnQuestion",btns)
-    btnTag.textContent = btns;
-    //btnTag.innerHTML = questionArray.btns
-    document.body.appendChild(btnTag);
-})
 
     // for (var i=0; i <  questionArray.length; i++) {
     //     var currentQuizQuestion = questionArray[0].question;
@@ -95,64 +182,3 @@ function questionButtonGenerator() {
     //     document.getElementById("questions").innerHTML = currentQuizQuestion
     // }
     //  for (var i=0; i <  questionArray.length; i++) {
-
-    //     var btn = document.createElement("BUTTON");
-    //     btn.setAttribute("class", btns)
-    //     document.body.appendChild(btn[i]);  
-     
-    //  }
-      
-     
-    //  document.body.appendChild(btn1);   
-    //  var btn2 = document.createElement("BUTTON");
-    //  document.body.appendChild(btn2);   
-    //  btn2.innerHTML = "button 2"
-    //  var btn3 = document.createElement("BUTTON");
-    //  document.body.appendChild(btn3);   
-    //  btn3.innerHTML = "button 3"
-    //  var btn4 = document.createElement("BUTTON");
-    //  document.body.appendChild(btn4);  
-    //  btn4.innerHTML = "button 4" 
-
-    // btnArray = [btn1, btn2, btn3, btn4];
-
-    //  currentQuizQuestion.forEach(btnArray => {
-    //     var btn = document.createElement("BUTTON");
-    //     btn.setAttribute("class", "choice");
-    //     btn.setAttribute("value", choice);
-
-    //  var questions = document.getElementById("quesions");
-    //  questions.innerHTML = alert('currentQuizQuestion');
-};
-
-
-function buttons() {
-
- //currentQuizQuestion = 1 ;
-
-    // var currentQuizQuestion = questionArray[currentQuestionIndex];
-    //     console.log(currentQuizQuestion);
-        
-
-    // currentQuizQuestion.btns.forEach(function(btns) {  // specifically for iterating over an array (.forEach)
-
-    //     console.log(btns);
-    //    // var button = questionArray.btns
-    //     console.log(btns);
-    //     var btnTag = document.createElement("BUTTON");
-    //     btnTag.setAttribute("btnQuestion",btns)
-    //     btnTag.textContent = btns;
-    //     //btnTag.innerHTML = questionArray.btns
-    //     document.body.appendChild(btnTag);
-    // })
-
-};
-
- 
-// startQuizBtn.addEventListener("click", quizQuestion);
- 
-
-// function click(){
-
-// };
-
